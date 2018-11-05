2ca8f2cd7ee851136a7495da6b8c5e5ae2f87c2d
Karan M Rajpal <rajpal.karan73@gmail.com>
2018-11-05 08:28:43 +0530

    [TEST] Added tests to check if feeds load on init

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -85,6 +85,7 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function(){
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -92,6 +93,15 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function (done) {
+            loadFeed(0, done);
+        });
+
+        it('are populated', function() {
+            const feed = document.querySelector('.feed');
+            expect(feed.children.length > 0).toBe(true);
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
